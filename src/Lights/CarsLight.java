bfc1456e1ee81c08a9a67e074a3ed76c80f4668b
David Meushar <mdavid10@gmail.com>
2018-05-06 20:05:58 +0300

    Yedidya is awesome

---
+++ src/Lights/CarsLight.java
@@ -0,0 +1,233 @@
+package Lights;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+
+public class CarsLight extends Thread
+{
+    Ramzor ramzor;
+    JPanel panel;
+    Event64 evTogreen,evToRed,evToShabat,evToChol,evAtRed;
+    int count;
+    private boolean stop=true;
+
+    enum OutState{ON_SHABAT,ON_CHOL}
+    enum InState{ON_RED,ON_GREEN,ON_ORANGE,ON_YELLOW,YELLOW_ON,YELLOW_OFF,GREEN_BLINK}
+    enum InInState{GREEN_ON,GREEN_OFF}
+    OutState outState;
+    InState inState;
+    InInState inInState;
+
+    public ShloshaAvot( Ramzor ramzor,JPanel panel,int key,Event64 evTogreen,Event64 evToRed,Event64 evToShabat,Event64 evToChol,Event64 evAtRed)
+    {
+        this.ramzor=ramzor;
+        this.panel=panel;
+        this.evTogreen=evTogreen;
+        this.evToRed=evToRed;
+        this.evToShabat=evToShabat;
+        this.evToChol=evToChol;
+        this.evAtRed=evAtRed;
+        start();
+    }
+
+    public void run()
+    {
+        try
+        {
+            outState=OutState.ON_CHOL;
+            while (true)
+            {
+                switch (outState) {
+                    case ON_CHOL:
+                        SetToRed();
+                        inState=InState.ON_RED;
+                        while(outState==OutState.ON_CHOL) {
+                            switch (inState) {
+                                case ON_RED:
+                                    while (true) {
+                                        if (evTogreen.arrivedEvent()) {
+                                            evTogreen.waitEvent();
+                                            SetToOrange();
+                                            inState = InState.ON_ORANGE;
+                                            break;
+                                        } else if (evToShabat.arrivedEvent()) {
+                                            evToShabat.waitEvent();
+                                            SetToGray();
+                                            outState = OutState.ON_SHABAT;
+                                            break;
+                                        } else
+                                            yield();
+                                    }
+                                    break;
+                                case ON_GREEN:
+                                    while (true) {
+                                        if (evToRed.arrivedEvent()) {
+                                            evToRed.waitEvent();
+                                            inState = InState.GREEN_BLINK;
+                                            break;
+                                        }
+                                        if (evToShabat.arrivedEvent()) {
+                                            evToShabat.waitEvent();
+                                            SetToGray();
+                                            outState = OutState.ON_SHABAT;
+                                            break;
+                                        } else
+                                            yield();
+                                    }
+                                    break;
+                                case ON_ORANGE:
+                                    while (true) {
+                                        if (evToShabat.arrivedEvent()) {
+                                            evToShabat.waitEvent();
+                                            SetToGray();
+                                            outState = OutState.ON_SHABAT;
+                                            break;
+                                        } else {
+                                            sleep(1000);
+                                            SetToGreen();
+                                            inState = InState.ON_GREEN;
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                case ON_YELLOW:
+                                    while (true) {
+                                        if (evToShabat.arrivedEvent()) {
+                                            evToShabat.waitEvent();
+                                            SetToGray();
+                                            outState = OutState.ON_SHABAT;
+                                            break;
+                                        } else {
+                                            sleep(1000);
+                                            SetToRed();
+                                            evAtRed.sendEvent();
+                                            inState = InState.ON_RED;
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                case GREEN_BLINK:
+                                    SetGreenOn();
+                                    count = 3;
+                                    inInState = InInState.GREEN_ON;
+                                    while (count > 3 && outState != OutState.ON_SHABAT) {
+                                        switch (inInState) {
+                                            case GREEN_ON:
+                                                if (evToShabat.arrivedEvent()) {
+                                                    evToShabat.waitEvent();
+                                                    outState = OutState.ON_SHABAT;
+                                                    SetToGray();
+                                                } else if (count > 0) {
+                                                    sleep(500);
+                                                    SetGreenOff();
+                                                    inInState = InInState.GREEN_OFF;
+                                                }
+                                                break;
+                                            case GREEN_OFF:
+                                                if (evToShabat.arrivedEvent()) {
+                                                    evToShabat.waitEvent();
+                                                    outState = OutState.ON_SHABAT;
+                                                    SetToGray();
+                                                } else {
+                                                    sleep(500);
+                                                    count--;
+                                                    SetGreenOn();
+                                                    inInState = InInState.GREEN_ON;
+                                                }
+                                                break;
+                                        }
+                                    }
+                                    SetToYellow();
+                                    inState = InState.ON_YELLOW;
+                                    break;
+                            }
+                        }
+                        break;
+                    case ON_SHABAT:
+                        SetYellowOn();
+                        inState=InState.YELLOW_ON;
+                        while (true)
+                        {
+
+                        }
+
+                }
+
+            }
+        } catch (InterruptedException e) {}
+
+    }
+    public void setLight(int place, Color color)
+    {
+        ramzor.colorLight[place-1]=color;
+        panel.repaint();
+    }
+
+    public boolean isStop()
+    {
+        return stop;
+    }
+
+    private void SetToGreen()
+    {
+        stop=false;
+        setLight(1,Color.LIGHT_GRAY);
+        setLight(2,Color.LIGHT_GRAY);
+        setLight(2,Color.GREEN);
+    }
+
+    private void SetToRed()
+    {
+        setLight(1,Color.RED);
+        setLight(2,Color.LIGHT_GRAY);
+        setLight(3,Color.LIGHT_GRAY);
+    }
+
+    private void SetToGray()
+    {
+        stop=true;
+        setLight(1,Color.LIGHT_GRAY);
+        setLight(2,Color.LIGHT_GRAY);
+        setLight(2,Color.LIGHT_GRAY);
+    }
+
+    private void SetToOrange() {
+        setLight(2,Color.YELLOW);
+    }
+
+    private void SetToYellow()
+    {
+        stop = true;
+        setLight(1,Color.LIGHT_GRAY);
+        setLight(2,Color.YELLOW);
+        setLight(3,Color.LIGHT_GRAY);
+    }
+
+    private void SetGreenOff()
+    {
+        setLight(3,Color.LIGHT_GRAY);
+    }
+
+    private void SetGreenOn()
+    {
+        setLight(3,Color.GREEN);
+    }
+
+    private void SetYellowOn()
+    {
+        setLight(2,Color.YELLOW);
+
+    }
+
+    private void SetYellowOff()
+    {
+        setLight(2,Color.LIGHT_GRAY);
+    }
+
+
+
+
+}
+
--- src/Lights/PeopleLight.java
+++ src/Lights/PeopleLight.java
@@ -1,5 +1,109 @@
 package Lights;
 
+import java.awt.Color;
+import javax.swing.JPanel;
+
+
 public class PeopleLight
 {
+    Ramzor ramzor;
+    JPanel panel;
+    Event64 evTogreen,evToRed,evToShabat,evToChol,evAtRed;
+
+    enum OutState{ON_SHABAT,ON_CHOL}
+    enum InState{ON_RED,ON_GREEN}
+    OutState outState;
+    InState inState;
+
+    public ShneyLuchot( Ramzor ramzor,JPanel panel,Event64 evTogreen,Event64 evToRed,Event64 evToShabat,Event64 evToChol,Event64 evAtRed )
+    {
+        this.ramzor=ramzor;
+        this.panel=panel;
+        this.evToChol=evToChol;
+        this.evTogreen=evTogreen;
+        this.evToRed=evToRed;
+        this.evToShabat=evToShabat;
+        this.evAtRed=evAtRed;
+        start();
+    }
+
+    public void run() {
+        try {
+            outState = OutState.ON_CHOL;
+            while (true) {
+                switch (outState) {
+                    case ON_CHOL:
+                        inState = InState.ON_RED;
+                        SetToRed();
+                        while (outState == OutState.ON_CHOL) {
+                            switch (inState) {
+                                case ON_GREEN:
+                                    while (true) {
+                                        if (evToRed.arrivedEvent()) {
+                                            evToRed.waitEvent();
+                                            SetToRed();
+                                            evAtRed.sendEvent();
+                                            inState = InState.ON_RED;
+                                            break;
+                                        } else if (evToShabat.arrivedEvent()) {
+                                            evToShabat.waitEvent();
+                                            SetToGray();
+                                            outState = OutState.ON_SHABAT;
+                                            break;
+                                        } else
+                                            yield();
+                                    }
+                                    break;
+                                case ON_RED:
+                                    while (true) {
+                                        if (evTogreen.arrivedEvent()) {
+                                            evTogreen.waitEvent();
+                                            SetToGreen();
+                                            inState = InState.ON_GREEN;
+                                            break;
+                                        } else if (evToShabat.arrivedEvent()) {
+                                            evToShabat.waitEvent();
+                                            SetToGray();
+                                            outState = OutState.ON_SHABAT;
+                                            break;
+                                        } else
+                                            yield();
+                                    }
+                                    break;
+                            }
+                        }
+                    case ON_SHABAT:
+                        evToChol.waitEvent();
+                        outState = OutState.ON_CHOL;
+                        break;
+                }
+            }
+        } catch (Exception e) {}
+    }
+
+
+    private void SetToGreen()
+    {
+        setLight(1,Color.GRAY);
+        setLight(2,Color.GREEN);
+    }
+
+    private void SetToRed()
+    {
+        setLight(1,Color.GRAY);
+        setLight(2,Color.GREEN);
+    }
+
+    private void SetToGray()
+    {
+        setLight(1,Color.GRAY);
+        setLight(2,Color.GRAY);
+    }
+
+    public void setLight(int place, Color color)
+    {
+        ramzor.colorLight[place-1]=color;
+        panel.repaint();
+    }
+
 }
