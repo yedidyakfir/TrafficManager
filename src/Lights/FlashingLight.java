83932ec73273d843bed7264a877e5c44763ed62b
David Meushar <mdavid10@gmail.com>
2018-05-06 19:26:59 +0300

    david is cool

--- src/Lights/FlashingLight.java
+++ src/Lights/FlashingLight.java
@@ -1,6 +1,54 @@
 package Lights;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class FlashingLight
 {
-    
+    Ramzor ramzor;
+    JPanel panel;
+    enum State {ON,OFF}
+    State state = State.OFF;
+
+    public Echad( Ramzor ramzor,JPanel panel)
+    {
+        this.ramzor=ramzor;
+        this.panel=panel;
+        start();
+    }
+
+    public void run()
+    {
+        try
+        {
+            SetOn();
+            while (true)
+            {
+                switch (state) {
+                    case ON:
+                        sleep(1000);
+                        SetOff();
+                        state=State.OFF;
+                        break;
+                    case OFF:
+                        sleep(1000);
+                        SetOn();
+                        state=State.ON;
+                        break;
+                }
+            }
+        } catch (InterruptedException e) {}
+
+    }
+    public void setLight(int place, Color color)
+    {
+        ramzor.colorLight[place-1]=color;
+        panel.repaint();
+    }
+    private void SetOn() {
+        setLight(1,Color.YELLOW);
+    }
+    private void SetOff() {
+        setLight(1,Color.GRAY);
+    }
 }
