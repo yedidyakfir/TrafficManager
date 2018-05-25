57bf9dcd1d34271571e8ce004594bfc6f9a04c08
yedidyakfir@gmail.com <yedidyakfir@gmail.com>
2018-05-25 12:44:56 +0300

    create groups for lights

--- src/CroosRoadControler.java
+++ src/CroosRoadControler.java
@@ -1,3 +1,5 @@
+import Lights.CarsLight;
+import Lights.PeopleLight;
 import Other.Event64;
 
 public class CroosRoadControler extends Thread
@@ -7,18 +9,27 @@ public class CroosRoadControler extends Thread
     Group outState;
     GroupState inState;
 
-    Integer[] GroupA;
-    Integer[] GroupB;
-    Integer[] GroupC;
+    CarsLight[] GroupA;
+    CarsLight[] GroupB;
+    CarsLight[] GroupC;
+
+    PeopleLight[] GroupApeople;
+    PeopleLight[] GroupBpeople;
+    PeopleLight[] GroupCpeople;
+
 
     Event64[] evTogreen ,evToRed , evToShabat,evToChol ,evAtRed;
 
-    public CroosRoadControler( Integer[] GroupA, Integer[] GroupB, Integer[] GroupC,Event64[] evTogreen,Event64[] evToRed,Event64[] evToShabat,Event64[] evToChol,Event64[] evAtRed)
+    public CroosRoadControler( CarsLight[] GroupA, CarsLight[] GroupB, CarsLight[] GroupC,PeopleLight[] GroupApeople,PeopleLight[] GroupBpeople,PeopleLight[] GroupCpeople,
+                               Event64[] evTogreen,Event64[] evToRed,Event64[] evToShabat,Event64[] evToChol,Event64[] evAtRed)
     {
 
         this.GroupA = GroupA;
         this.GroupB = GroupB;
         this.GroupC = GroupC;
+        this.GroupApeople = GroupApeople;
+        this.GroupBpeople = GroupBpeople;
+        this.GroupCpeople = GroupCpeople;
         this.evTogreen=evTogreen;
         this.evToRed=evToRed;
         this.evToShabat=evToShabat;
